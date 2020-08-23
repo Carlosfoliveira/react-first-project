2b012ee1699864ac63357dc74c63e5295d0642e6
carlosfoliveira <carlos.fe.oliveira@gmail.com>
2020-08-23 14:43:59 -0300

    11 - Navigating routes

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 import api from '../../services/api';
 
@@ -68,7 +69,7 @@ const Main = () => {
         {repositories.map((repository) => (
           <li key={repository.name}>
             <span>{repository.name}</span>
-            <a href="">Detalhes</a>
+            <Link to={`/repository/${encodeURIComponent(repository.name)}`}>Detalhes</Link>
           </li>
         ))}
 
