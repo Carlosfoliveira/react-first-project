3414ce96172c30e5c3fbd53859bdef81b8472e32
carlosfoliveira <carlos.fe.oliveira@gmail.com>
2020-08-24 08:27:52 -0300

    12 - Loading api data

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from '../../services/api';
 
 // import { Container } from './styles';
 
-function Repository({ match }) {
+const Repository = ({ match }) => {
+  const [repository, setRepository] = useState({});
+  const [issues, setIssues] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchData() {
+      const repoName = decodeURIComponent(match.params.repository);
+
+      const [repositoryFromApi, issuesFromApi] = await Promise.all([
+        api.get(`/repos/${repoName}`),
+        api.get(`/repos/${repoName}/issues`, {
+          params: {
+            state: 'open',
+            per_page: 5,
+          },
+        }),
+      ]);
+
+      setRepository(repositoryFromApi.data);
+      setIssues(issuesFromApi.data);
+      setLoading(false);
+
+      console.log(repositoryFromApi);
+      console.log(issuesFromApi);
+    }
+    fetchData();
+  }, []);
+
   return (
     <h1>
-      Repository:
-      {' '}
-      {decodeURIComponent(match.params.repository)}
+      Repository
     </h1>
   );
-}
+};
 
 export default Repository;
